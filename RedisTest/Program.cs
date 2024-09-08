804ec68bfac9b853930f7edaaa41e2ab9d7c26eb
ken <work970390@gmail.com>
2024-09-08 15:43:59 +0800

    init

---
+++ RedisTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+var redis = ConnectionMultiplexer.Connect("172.20.128.1:6379");
+var db = redis.GetDatabase();
+
+var stopwatch = new Stopwatch();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+stopwatch.Start();
+var redisKey = "840";
+
+for (int i = 0; i < 20_000; i++)
+{
+    await db.HashExistsAsync(redisKey, "field"+i);
+    // await db.HashGetAsync(redisKey, "field"+i);
+    // await db.HashSetAsync(redisKey, $"field{i}",i.ToString());
+}
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+
